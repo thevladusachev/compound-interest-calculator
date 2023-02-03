e954b6a99ceb3c96cef5512eadebfd5f32a05a66
Vlad Usachev <Vladuschv@yandex.ru>
2023-02-03 15:47:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FormOfCalculation from './components/FormOfCalculation/FormOfCalculation';
+import Header from './components/Header';
+import Title from './components/Title/Title';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="AppWrapper">
+			<Header />
+			<Title title='Калькулятор доходности вклада онлайн' />
+			<FormOfCalculation />
+		</div>
+	);
 }
 
 export default App;
